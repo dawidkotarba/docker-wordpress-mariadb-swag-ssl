a6fd9226122eddb95195066d2f844090780ac66f
dawidkotarba <dawidkotarba>
2022-04-28 17:32:14 +0200

    Initial commit

---
+++ install-docker.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
+newgrp docker
+
+sudo systemctl start docker
+sudo systemctl enable docker
